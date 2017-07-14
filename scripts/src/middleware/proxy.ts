8eff498acddf7736916c019fe00b036cb25ea651
Ehud Hoggeg <ehud.hoggeg@lightcyber.com>
2017-07-14 15:07:14 +0300

    middleware for oauth2 (wip)

--- scripts/src/middleware/proxy.ts
+++ scripts/src/middleware/proxy.ts
@@ -5,17 +5,11 @@
 import * as express from "express";
 import * as httpProxy from "http-proxy";
 
-import { writeCorsHeaders } from "../utils";
-
 let proxy: httpProxy.ProxyServer;
 
 export function middleware(remoteOrigin: string) {
 	proxy = httpProxy.createProxyServer({
 		target: remoteOrigin
-	});
-
-	proxy.on("proxyRes", (proxyRes, request, response) => {
-		writeCorsHeaders(response);
 	});
 
 	return function handler(request: express.Request, response: express.Response) {
